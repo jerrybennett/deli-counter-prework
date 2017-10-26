f9541f72aa10fc3c6811ca1e5e2e1de9ffad2c8d
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-26 21:36:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(arr)
   if arr.length == 0
     puts "The line is currently empty"
   else
-    puts "The line is currently " 
+    arr.each_with_index do |name, num|
+    puts "The line is currently #{num}. #{name}" 
   end
 end
